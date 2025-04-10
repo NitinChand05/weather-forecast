1f12b43e7cf2ca69e20a34b8af6c8b6c55a40faf
Nitin Chand <nitin.chand@trifler.in>
2025-04-10 17:47:36 +0530

    Checking the branch

--- src/main/java/MyPackage/Check.java
+++ src/main/java/MyPackage/Check.java
@@ -3,5 +3,7 @@ package MyPackage;
 public class Check {
     public static  void main(String[]args){
         System.out.println("its just to check the result ");
+        System.out.println("Checking the branch work ");
+
     }
 }
